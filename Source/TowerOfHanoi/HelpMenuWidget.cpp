0372f13ea2d37f0af218f982febdbe138dd75847
Akie <legaspiernstdanielle@gmail.com>
2023-02-17 03:39:52 +0800

    Added more help menu text
    Added more help menu text

--- Source/TowerOfHanoi/HelpMenuWidget.cpp
+++ Source/TowerOfHanoi/HelpMenuWidget.cpp
@@ -14,18 +14,22 @@ void UHelpMenuWidget::NextButtonOnClicked() {
 	switch(DialogStage) {
 		case EDialogStage::First:
 			DialogStage = EDialogStage::Second;
-			HelpText->SetText(FText::FromString("As you may know, tower of hanoi consists of three towers (pegs) and more than one rings."));
+			HelpText->SetText(FText::FromString("Press R to restart the game and press T to quit the game. Left click on the ground to move."));
 			break;
 		case EDialogStage::Second:
 			DialogStage = EDialogStage::Third;
-			HelpText->SetText(FText::FromString("In this game, the three towers are replaced by a yard (blocked by fence in a square shaped). and rings are replaced by wood logs."));
+			HelpText->SetText(FText::FromString("As you may know, tower of hanoi consists of three towers (pegs) and more than one rings."));
 			break;
 		case EDialogStage::Third:
 			DialogStage = EDialogStage::Fourth;
-			HelpText->SetText(FText::FromString("A stone is placed in front of the yard. Step on this stone to get a wood log and step again to another stone to place the wood log."));
+			HelpText->SetText(FText::FromString("In this game, the three towers are replaced by a yard (blocked by fence in a square shaped). And rings are replaced by wood logs."));
 			break;
 		case EDialogStage::Fourth:
-			HelpText->SetText(FText::FromString("Place all the logs in the third yard (rightmost yard) in ascending order just like on how you will play tower oh hanoi. good luck and have fun!"));
+			DialogStage = EDialogStage::Fifth;
+			HelpText->SetText(FText::FromString("A stone is placed in front of the yard. Step on this stone to get a wood log and step again to another stone to place the wood log."));
+			break;
+		case EDialogStage::Fifth:
+			HelpText->SetText(FText::FromString("Place all the logs in the third yard (rightmost yard) in ascending order just like on how you will play tower oh hanoi. Good luck and have fun!"));
 			DialogStage = EDialogStage::Default;
 			break;
 		default:
--- Source/TowerOfHanoi/HelpMenuWidget.h
+++ Source/TowerOfHanoi/HelpMenuWidget.h
@@ -10,7 +10,8 @@ enum class EDialogStage : uint8 {
 	First = 1,
 	Second = 2,
 	Third = 3,
-	Fourth = 4
+	Fourth = 4,
+	Fifth = 5
 };
 
 UCLASS()
